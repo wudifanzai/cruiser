c08d5e2305190e391da87433f4699c17e9479337
me <1564658292@qq.com>
2018-03-28 00:02:07 +0800

    first commit

---
+++ src/main/java/com/fanzai/SpringcludeTestApplication.java
@@ -0,0 +1,12 @@
+package com.fanzai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcludeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcludeTestApplication.class, args);
+	}
+}
